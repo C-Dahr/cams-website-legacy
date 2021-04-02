b0a20b9dcc91ffe473155bb1a156c2fc0640dffb
C-Dahr <cameronsdahr@gmail.com>
2021-04-02 04:08:39 -0300

    Added Vuetify

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
@@ -13,14 +14,17 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
+  },
+  {
+    path: '/',
+    name: 'Default',
+    component: Home
   }
 ]
 
 const router = new VueRouter({
+  mode: "history",
   routes
 })
 
