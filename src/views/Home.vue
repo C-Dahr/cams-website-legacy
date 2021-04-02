27624e23b8e182ebeddf147d715c71ca1f3bc845
C-Dahr <cameronsdahr@gmail.com>
2021-04-02 05:58:36 -0300

    Added social media buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,17 @@
 					<h1 class="white--text text-xl-h1">Cameron Dahr</h1>
 					<v-divider class="mt-5 mb-5" color="white" width="20%"></v-divider>
 					<h3 class="white--text text-xl-h4">Full Stack Software Developer</h3>
+					<v-row cols="2" class="mt-8" align="center" justify="space-between" style="max-width: 10%">
+						<v-btn icon target="_blank" href="https://github.com/C-Dahr">
+							<v-icon large color="white">fab fa-github-square</v-icon>
+						</v-btn>
+						<v-btn icon target="_blank" href="https://www.linkedin.com/in/cameron-dahr-8a943a129/">
+							<v-icon large color="white">fab fa-linkedin</v-icon>
+						</v-btn>
+						<v-btn icon target="_blank" href="https://www.facebook.com/cameron.dahr.1">
+							<v-icon large color="white">fab fa-facebook-square</v-icon>
+						</v-btn>
+					</v-row>
 				</v-col>
 			</transition>
 		</v-row>
