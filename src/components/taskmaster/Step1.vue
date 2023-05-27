57706ccb09adc44c81a16da5fbd1c2d3ce5b9c22
C-Dahr <cameronsdahr@gmail.com>
2023-05-27 03:57:35 -0300

    Added step 2

---
+++ src/components/taskmaster/Step1.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-card dark max-width="50%" color="rgb(0, 0, 0, 0.7)">
+        <v-card-title class="white--text text-xl-h4">
+            First things first
+        </v-card-title>
+        <v-card-text class="white--text text-lg-h5 text-left" style="line-height: 2;">
+            I'd love to get to know you better. What's your name?
+        </v-card-text>
+        <v-row justify="center">
+            <v-col cols="8">
+                <v-text-field v-model="username" label="Name"></v-text-field>
+            </v-col>
+        </v-row>
+        <v-card-actions class="justify-center pb-5">
+            <v-btn
+                x-large
+                light 
+                @click="step1"
+            >
+                Submit
+            </v-btn>
+        </v-card-actions>
+        </v-card>
+</template>
+
+<script>
+
+export default {
+    name: 'Step1',
+    components: {
+    },
+    data() {
+		return {
+            username: ''
+		}
+	},
+    methods: {
+        step1() {
+            this.$parent.step1(this.username);
+        }
+    }
+};
+
+</script>
+
+<style>
+
+</style>
--- src/views/Taskmaster.vue
+++ src/views/Taskmaster.vue
@@ -6,16 +6,28 @@
 					<v-col cols="2" class="headshot">
 						<v-img src="../assets/Taskmaster_logo.jpg" alt="Cameron"></v-img>
 					</v-col>
-					<v-col  cols="4" align="center">
+					<v-col align="center" cols="4">
 						<h1 class="white--text text-xl-h1">Taskmaster</h1>
 						<v-divider class="mt-5 mb-5" color="white" width="20%"></v-divider>
 						<h3 class="white--text text-xl-h4">Welcome Contestants</h3>
 					</v-col>
 				</v-row>
-				<v-row class="mt-15" justify="center">
+				<v-row class="mt-15" align="center" justify="center">
 					<transition name="fade">
 						<Step0 v-if="step==0"/>
 					</transition>
+					<transition name="fade">
+						<Step1 v-if="step==1"/>
+					</transition>
+				</v-row>
+				<v-row class="mt-15" align="center" justify="center">
+					<v-btn
+					x-large
+					dark 
+					@click="reset"
+					>
+						Reset
+					</v-btn>
 				</v-row>
 			</v-col>
 		</v-row>
@@ -24,23 +36,44 @@
 
 <script>
 import Step0 from '../components/taskmaster/Step0.vue'
+import Step1 from '../components/taskmaster/Step1.vue'
 
 export default {
 	name: 'Home',
 	components: {
-		Step0
+		Step0,
+		Step1,
+	},
+	created() {
+		var storedStep = localStorage.getItem('TaskmasterStep')
+		console.log(storedStep)
+		if (storedStep != null){
+			this.step == storedStep
+		}else{
+			this.step = 0
+		}
+		console.log(this.step)
 	},
 	data() {
 		return {
-			step: 0
+			step: localStorage.getItem('TaskmasterStep')
 		}
 	},
 	methods: {
 		ready() {
 			this.step=1
+			localStorage.setItem("TaskmasterStep", this.step)
 		},
-		step1() {
-			
+		step1(username) {
+			localStorage.setItem("Username", username)
+			this.step=2
+			localStorage.setItem("TaskmasterStep", this.step)
+			console.log(localStorage.getItem("Username"))
+		},
+		reset() {
+			localStorage.setItem("Username", "")
+			this.step=0
+			localStorage.setItem("TaskmasterStep", this.step)
 		}
 	}
 }
@@ -53,9 +86,14 @@ export default {
 	background-image: url('../assets/homepage-tile.png') !important;
 }
 
-.fade-enter-active, .fade-leave-active {
+.fade-enter-active {
   transition: opacity 2s;
 }
+
+.fade-leave-active {
+  transition: opacity 0s;
+}
+
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
